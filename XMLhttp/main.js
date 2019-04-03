d027d4d14f221ce35f4687eb84c8bba38e4bee0a
Lee <787441351@qq.com>
2019-04-03 14:22:00 +0800

    111

--- XMLhttp/main.js
+++ XMLhttp/main.js
@@ -1,7 +1,8 @@
 myButton.addEventListener('click',(e)=>{
     let request = new XMLHttpRequest();
     request.open('GET','/xxx');
-    request.send();
+    request.setRequestHeader('frank','18');
+    request.send('发送内容');
     request.onreadystatechange=()=>{
         // request.readyState 状态码
         if(request.readyState===4){
@@ -18,6 +19,9 @@ myButton.addEventListener('click',(e)=>{
                 // 处理json
                 let object = window.JSON.parse(string);
                 console.log(object);
+                console.log(request.statusText);
+                console.log(request.getResponseHeader('content-Type'))
+                console.log(request.getAllResponseHeaders())
 
             }else if(request.status >= 400){
                 console.log('请求失败')
--- main.js
+++ main.js
@@ -1,69 +1,9 @@
 myButton.addEventListener('click', (e) => {
-
-    window.jQuery.ajax({
+    $.ajax({
         url: '/xxx',
         method: 'post',
-        body: 'a=1&b=2',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'frank': '18'
-        },
-        successFn: (x) => {
-            console.log(x)
-        },
-        failFn: (x) => {
-            console.log(x.status);
-            console.log(x.responseText)
-            f1(x);
-        }
-    })
+
+    }).then((responseText)=>{console.log(responseText)},
+        (request)=>{console.log('error')})
 });
-function f1(request){}
 
-
-window.jQuery = function (nodeOrSelector) {
-    let nodes = {};
-    nodes.addClass = function () {
-    };
-    nodes.html = function () {
-    };
-    return nodes;
-};
-
-window.jQuery.ajax = function ({url,method,body,success,fail,headers}) {
-    //多个参数写法
-    // let url;
-    // if(arguments.length ===1){
-    //     url = options.url;
-    // }else if(arguments.length ===2){
-    //     url = arguments[0];
-    //     options = arguments[1];
-    // }
-
-    let request = new XMLHttpRequest();
-    if (Math.random() > 0.5) {
-        request.open(method, url);
-    } else {
-        request.open(method, '/yyy');
-    }
-    for (let key in headers) {
-        let value = headers[key];
-        request.setRequestHeader(key, value);
-    }
-
-    request.onreadystatechange = () => {
-        // request.readyState 状态码(1-4)
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                success.call(undefined, request.responseText)
-            } else if (request.status >= 400) {
-                fail.call(undefined, request)
-            }
-        } else {
-
-        }
-    };
-    request.send(body);
-};
-
-window.$ = window.jQuery;
